9a8f15db28b48a95d5042c2fdd22f12c936a4a0a
Herbert Ansong Koranteng <101828120+H-erbie@users.noreply.github.com>
2024-11-26 15:12:44 +0000

    Update market-story-card.jsx

--- components/market-story-card.jsx
+++ components/market-story-card.jsx
@@ -4,7 +4,7 @@ const MarketStoryCard = ({id, thumbnail,headline, description }) => {
   
     return (
     <div className='my-8 shadow-lr'>
-        <Image src={thumbnail} alt={id} width={250} height={200} className='w-full bg-gray-400 '/>
+        <Image src={thumbnail} alt={id} width={250} sizes="responsive" height={200} className='w-full bg-gray-400 '/>
         <div className="p-3">
         <h3 className='text-lg text-gray-700 capitalize font-bold'>{headline}</h3>
         <p className='text-sm'>{description.slice(0,105)}...</p>
